2c6748fd6998ee15f15d8d07fb748a64a84628c9
Martin Nowak <martinnowak96@outlook.com>
2019-11-06 13:36:26 -0500

    nn

--- MachineLearning/waveformsML.py
+++ MachineLearning/waveformsML.py
@@ -13,9 +13,9 @@ import os
 MODEL_NAME = "Frequency-{}".format(int(time.time()))
 tensorboard = TensorBoard(log_dir=f'logs\\{MODEL_NAME}')
 
-frameSize = 150
-timeFrame = "750"
-inputData,outputData = getData.getMachineLearningDataWaveforms(frameSize)
+frameCount = 10000
+timeFrame = "500"
+inputData,outputData = getData.getMachineLearningDataWaveforms(frameCount)
 print(inputData.shape)
 
 trainInput, testInput, trainOutput, testOutput = sk.train_test_split(inputData,outputData,test_size=0.1, random_state = 42)
@@ -23,9 +23,9 @@ trainInput, testInput, trainOutput, testOutput = sk.train_test_split(inputData,o
 
 model = keras.Sequential([
     
-    keras.layers.GRU(50, activation='tanh', recurrent_activation='sigmoid',input_shape=(frameSize, 3), use_bias=True, kernel_initializer='glorot_uniform', recurrent_initializer='orthogonal', bias_initializer='zeros', kernel_regularizer=None, recurrent_regularizer=None, bias_regularizer=None, activity_regularizer=None, kernel_constraint=None, recurrent_constraint=None, bias_constraint=None, dropout=0.0, recurrent_dropout=0.0, implementation=2, return_sequences=False, return_state=False, go_backwards=False, stateful=False, unroll=False, reset_after=False),
+    keras.layers.GRU(25, activation='tanh', recurrent_activation='sigmoid',input_shape=(frameCount, 3), use_bias=True, kernel_initializer='glorot_uniform', recurrent_initializer='orthogonal', bias_initializer='zeros', kernel_regularizer=None, recurrent_regularizer=None, bias_regularizer=None, activity_regularizer=None, kernel_constraint=None, recurrent_constraint=None, bias_constraint=None, dropout=0.0, recurrent_dropout=0.0, implementation=2, return_sequences=False, return_state=False, go_backwards=False, stateful=False, unroll=False, reset_after=False),
     keras.layers.Flatten(),
-    keras.layers.Dense(400, activation=tf.nn.relu),
+    keras.layers.Dense(300, activation=tf.nn.relu),
     keras.layers.GaussianDropout(0.01),
     keras.layers.Dense(200, activation=tf.nn.relu),
     keras.layers.GaussianDropout(0.01),
