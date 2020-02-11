882f6f8528dea75220c527d2087db9e75e5da067
Martin Nowak <martinnowak96@outlook.com>
2020-02-11 14:19:48 -0500

    removed junk

--- MachineLearning/getData.py
+++ MachineLearning/getData.py
@@ -18,47 +18,6 @@ def natural_keys(text):
     '''
     return [ atoi(c) for c in re.split(r'(\d+)', text) ]
 
-
-
-def getData(folderName):
-    onlyfiles = [f for f in listdir("./data/" +folderName) if isfile(join("./data/" + folderName, f))]
-
-    fileCount = 0
-    x = []
-    y_on= []
-    y_off = []
-    y_all = []
-    for file in onlyfiles:
-        with open('./data/'+folderName+"/" +file, 'r') as csvfile:
-            fileCount +=1
-            reader = csv.reader(csvfile, delimiter=',')
-            i =0
-            for row in reader:
-                if i != 0:
-                    x.append((i-1) *1500*0.000001)
-                    y_all.append(int(row[2]))
-                    y_off.append(int(row[1]))
-                    y_on.append(int(row[0]))
-                i+=1
-                
-    N= fileCount
-    return y_on,y_off,y_all,N,x
-
-
-def getEventChunkData(folderName):
-    points = []
-    onlyfiles = [f for f in listdir("./eventChunkData/" +folderName) if isfile(join("./eventChunkData/" + folderName, f))]
-    for file in onlyfiles:
-        with open('./eventChunkData/'+folderName+"/" +file, 'r') as csvfile:
-            
-            reader = csv.reader(csvfile, delimiter=',')
-            i =0
-            for row in reader:
-                if i != 0:
-                    points.append([int(row[1]), 128-int(row[2])])
-                i+=1
-        return points  
-
 def getMachineLearningData(numberOfFrames):
     allInputData = []#numberOfFrames x 3
     allOutputData = []#frequency
