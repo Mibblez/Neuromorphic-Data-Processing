3f439d54c38964418c6be8c03ad46662b97e29e7
Mibblez <abeninati01@manhattan.edu>
2020-06-04 16:25:57 -0400

    Update fingerprint plot

--- plotting/eventCountLogScale.py
+++ plotting/eventCountLogScale.py
@@ -1,28 +1,25 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.mlab as mlab
-from matplotlib import pyplot
-import matplotlib
-from matplotlib.ticker import ScalarFormatter, FormatStrFormatter
-import getPlottingData
 import os
-from os import walk
-from os import listdir
-from os.path import isfile, join
 import matplotlib.ticker as mticker
+import matplotlib.pyplot as plt
+from matplotlib.ticker import ScalarFormatter
+import getPlottingData
 
-# TODO: automatically streatch graph along x-axis 
-folderName = "5hz Pol (1200 Reconstruction Window)"
+hz = 40
+folder_name = f"no_pol/Front{hz}_Rear0 Event Chunks"
+plot_name = f"{hz}hz Not Polarized Fingerprint (1200μs Reconstruction Window)"
+save_fig = True
+show_fig = False
+
 config = getPlottingData.parseConfig()
-test =  getPlottingData.getData(os.path.join(config.dataFolder,folderName), config.reconstructionWindow, config.maxEventCount)
+plot_data = getPlottingData.getData(os.path.join(config.dataFolder, folder_name), config.reconstructionWindow, config.maxEventCount)
 
-all_event_counts = test[2]
-x = test[4]
+all_event_counts = plot_data[2]
+x = plot_data[4]
 
-plt.title(folderName)
-plt.plot(x, all_event_counts, '-o')
+plt.title(plot_name)
+plt.plot(x, all_event_counts, '-o', markersize=4)
 
-pyplot.yscale('log', basey=10)
+plt.yscale('log', basey=10)
 
 ax = plt.gca()
 
@@ -32,4 +29,10 @@ ax.yaxis.set_minor_formatter(mticker.ScalarFormatter())
 plt.xlabel('Time (seconds)')
 plt.ylabel('Event Count')
 
-plt.show()
+plt.gcf().set_size_inches((10, 4.5))
+
+if save_fig:
+    plt.savefig(os.path.join(f'{plot_name}.png'))
+
+if show_fig:
+    plt.show()
