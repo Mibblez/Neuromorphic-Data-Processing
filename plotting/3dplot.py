f5610696b89606b7d7cb8b81ca6d3354ceb21b92
Anthony Beninati <abeninati01@manhattan.edu>
2020-06-23 22:56:19 -0400

    Simplify 3dplot

--- plotting/3dplot.py
+++ plotting/3dplot.py
@@ -1,5 +1,3 @@
-from os import listdir
-from os.path import isfile, join
 import argparse
 import os
 import csv
@@ -7,8 +5,6 @@ import itertools
 
 import matplotlib.pyplot as plt
 
-show_plot = True
-save_fig = False
 file_to_plot = ''
 view = None
 
@@ -18,14 +14,15 @@ def get_aedat_csv_data(csv_file):
 
     with open(csv_file, 'r') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
-
         next(reader, None) # Skip header
 
-        first_row = next(reader, None)
-        first_timestamp = int(first_row[3])
+        reader_list = list(reader)
 
-        for row in itertools.chain([first_row], reader):
-            polarity = row[0] == "True"
+        # Get the first timestamp from the fourth column of the first entry
+        first_timestamp = int(reader_list[0][3])
+
+        for row in reader_list:
+            polarity = row[0] in ['1', 'True']
             x_pos = int(row[1])
             y_pos = 128 - int(row[2])
             timestamp = int(row[3]) - first_timestamp
@@ -35,27 +32,27 @@ def get_aedat_csv_data(csv_file):
     return points
 
 def get_args():
-    global show_plot, save_fig, file_to_plot, view
+    global file_to_plot, view
 
     parser = argparse.ArgumentParser()
 
     parser.add_argument("aedat_csv_file", help='CSV containing AEDAT data to be plotted', type=str)
-    parser.add_argument('--hide_plot', '-hp', help='prevents the plot from being shown', action='store_true')
-    parser.add_argument('--save_fig', '-sf', help='saves the figure to disk', action='store_true')
-    parser.add_argument('--view_angle', '-v', help='sets plot viewing angle [default, top, side]', action='store', type=str)
+    parser.add_argument('--view_angle', '-v', help='sets plot viewing angle [default, top, side, all]', action='store', type=str)
 
     args = parser.parse_args()
 
-    show_plot = not args.hide_plot
-    save_fig = args.save_fig
     file_to_plot = args.aedat_csv_file
 
-    viewing_angles = ['default', 'top', 'side']
+    viewing_angles = ['default', 'top', 'side', 'all']
 
     if args.view_angle is not None:
         view = args.view_angle.lower()
         if view not in viewing_angles:
-            print('Invalid view. Using default instead.')
+            quit('Invalid view. Use one of the following: [default, top, side, all]')
+    else:
+        print('usage: 3dplot.py [-h] [--view_angle VIEW_ANGLE] aedat_csv_file')
+        print('3dplot.py: error: the following arguments are required: view_angle')
+        quit('Use one of the following view angles: [default, top, side, all]')
 
 
 if __name__ == '__main__':
@@ -77,30 +74,33 @@ if __name__ == '__main__':
             color.append("r")
 
     fig = plt.figure()
+    fig.set_size_inches(12, 10)
     ax = fig.add_subplot(111, projection='3d')
     ax.set_xlabel('X Position')
     ax.set_ylabel('Y Position')
     ax.set_zlabel('Time (μs)')
 
-    if view == 'top':
-        ax.scatter(all_x, all_y, all_time, c=color, marker='H', s=4, depthshade=False)
-        ax.set_zticklabels([])
-        ax.view_init(azim=-90, elev=87)
-    elif view == 'side':
-        ax.scatter(all_x, all_y, all_time, c=color, marker='H', s=4, depthshade=False)
-        ax.view_init(azim=0, elev=8)
-    else:
-        ax.scatter(all_x, all_y, all_time, c=color, marker='.', s=4, depthshade=False)
-
-    fig.set_size_inches(12, 10)
-
     file_name = os.path.basename(os.path.normpath(file_to_plot))    # Get file at end of path
     file_name = os.path.splitext(file_name)[0]                      # Strip off file extension
 
-    if save_fig:
-        plt.savefig(os.path.join(f'3D_Plot-{file_name}.png'))
+    if view in ['default', 'all']:
+        ax.scatter(all_x, all_y, all_time, c=color, marker='.', s=4, depthshade=False)
 
-    if show_plot:
-        plt.show()
-    else:
-        plt.clf()
+        fig.savefig(os.path.join(f'3D_Plot-{file_name}-default.png'), bbox_inches='tight', pad_inches=0)
+
+
+    if view in ['side', 'all']:
+        ax.scatter(all_x, all_y, all_time, c=color, marker='H', s=4, depthshade=False)
+        ax.view_init(azim=0, elev=8)
+
+        fig.savefig(os.path.join(f'3D_Plot-{file_name}-side.png'), bbox_inches='tight', pad_inches=0)
+
+
+    if view in ['top', 'all']:
+        ax.scatter(all_x, all_y, all_time, c=color, marker='H', s=4, depthshade=False)
+        ax.set_zticklabels([])
+        ax.view_init(azim=-90, elev=87)
+
+        fig.savefig(os.path.join(f'3D_Plot-{file_name}-top.png'), bbox_inches='tight', pad_inches=0)
+
+    plt.clf()
