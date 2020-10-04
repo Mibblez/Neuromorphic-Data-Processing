4c47fb0667914398c84abb797f94af2f58ddfe76
Anthony Beninati <abeninati01@manhattan.edu>
2020-10-03 21:17:42 -0400

    update output file location for mean_shift_segmentation.py

--- image_processing/mean_shift_segmentation.py
+++ image_processing/mean_shift_segmentation.py
@@ -15,13 +15,13 @@ def get_args():
 
     arg_path = args.image_path
 
-def mss_and_save(image_path):
+def mss_and_save(image_path, save_folder=''):
     original_image = Image.open(image_path)
 
-    # Mean shift segmentation
+    # Set MSS values
     spatial_radius = 6
     range_radius = 4.5
-    min_density = 50
+    min_density = 5
 
     pms_segmenter = pms.Segmenter()
     pms_segmenter.spatial_radius = spatial_radius
@@ -30,8 +30,9 @@ def mss_and_save(image_path):
 
     (segmented_image, labels_image, number_regions) = pms_segmenter(original_image)
 
-    output_filename = f"{os.path.splitext(image_path)[0]}_mss.png"
-    status = cv2.imwrite(output_filename, segmented_image)
+    # Save segmented image to file
+    output_filename = f"{os.path.basename(os.path.splitext(image_path)[0])}_mss_d{min_density}.png"
+    status = cv2.imwrite(os.path.join(save_folder, output_filename), segmented_image)
 
 if __name__ == '__main__':
     get_args()
@@ -39,7 +40,14 @@ if __name__ == '__main__':
     if os.path.isfile(arg_path):
         mss_and_save(arg_path)
     else:
+        output_folder = f"{os.path.basename(os.path.normpath(arg_path))}_mss"
+
+        if not os.path.exists(output_folder):
+            os.mkdir(output_folder)
+        else:
+            quit(f"Error: output directory {output_folder} already exists")
+
         for this_file in os.listdir(arg_path):
             if this_file.endswith(('.png', '.jpg', '.jpeg')):
                 full_path = os.path.join(arg_path, this_file)
-                mss_and_save(full_path)
+                mss_and_save(full_path, output_folder)
