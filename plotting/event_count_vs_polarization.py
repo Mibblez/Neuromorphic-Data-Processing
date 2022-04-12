1e4847478a159bef75a2faed8f73c54d60debea8
Anthony Beninati <abeninati01@manhattan.edu>
2022-04-12 18:49:33 -0400

    Add progress bar

--- plotting/event_count_vs_polarization.py
+++ plotting/event_count_vs_polarization.py
@@ -1,3 +1,4 @@
+import imp
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FormatStrFormatter, AutoMinorLocator
 import argparse
@@ -5,21 +6,27 @@ import re
 import os
 from natsort import natsorted
 import pandas as pd
+import tqdm
 
 csv_folder = None
+debug_info = False
 
 
 def get_args():
-    global csv_folder
+    global csv_folder, debug_info
 
     parser = argparse.ArgumentParser()
     parser.add_argument("csv_folder", help="Folder with CSV files to plot", type=str)
+    parser.add_argument("--debug_info", '-d', help="Display debug info insead of a progress bar",
+                        action='store_true')
     args = parser.parse_args()
 
     csv_folder = args.csv_folder
 
     if not os.path.exists(csv_folder) or not os.path.isdir(csv_folder):
         quit(f"The folder {csv_folder} does not exist")
+
+    debug_info = args.debug_info
 
 
 if __name__ == "__main__":
@@ -28,18 +35,28 @@ if __name__ == "__main__":
     # Resize plot
     plt.rcParams['figure.figsize'] = [11, 5.5]
 
+    if debug_info:
+        pbar = natsorted(os.listdir(csv_folder))
+    else:
+        pbar = tqdm.tqdm(natsorted(os.listdir(csv_folder)))
+
     # Iterate over sub-directories inside csv_folder
-    for current_folder in natsorted(os.listdir(csv_folder)):
+    for current_folder in pbar:
         if not os.path.isdir(os.path.join(csv_folder, current_folder)):
             continue
 
         hz = re.search("[0-9]{1,} ?[H|h][Z|z]", current_folder)
 
         if not hz:
-            print(f"Could not parse frequency for folder '{current_folder}', skipping...")
+            if debug_info:
+                print(f"Could not parse frequency for folder '{current_folder}', skipping...")
             continue
 
         hz = hz.group()
+
+        # Update progress bar if it is being used
+        if type(pbar) == tqdm.std.tqdm:
+            pbar.set_description(f"Processing {hz}")
 
         plot_x = []
         plot_y = []
@@ -57,7 +74,8 @@ if __name__ == "__main__":
             if degrees:
                 degrees = int(re.search("[0-9]{1,}", degrees.group()).group())
             else:
-                print(f"Could not parse polarization angle for file '{full_csv_path}', skipping...")
+                if debug_info:
+                    print(f"Could not parse polarization angle for file '{full_csv_path}', skipping...")
                 continue
 
             df = pd.read_csv(full_csv_path, header=0, usecols=['Timestamp'])
