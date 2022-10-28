4d00d8610fb14164841f94d323b4269da4f027c1
ykopchak <90361185+ykopchak@users.noreply.github.com>
2022-10-28 14:24:33 -0400

    Update test_get_plotting_data.py (#29)

--- tests/test_get_plotting_data.py
+++ tests/test_get_plotting_data.py
@@ -1,11 +1,13 @@
 # import pytest
 from plotting_utils import get_plotting_data
 from plotting_utils.get_plotting_data import DataStorage
+import pytest
 
 
 def test_spatial_csv_bool_no_color():
     spatial_csv_data = get_plotting_data.SpatialCsvData.from_csv(
-        "tests/test_data/OnOff-X-Y-Timestamp.csv", DataStorage.BOOL)
+        "tests/test_data/OnOff-X-Y-Timestamp.csv", DataStorage.BOOL
+    )
 
     assert spatial_csv_data.polarities == [True, False, False, True, True, True, True, True, False, False]
     assert spatial_csv_data.x_positions == [82, 17, 86, 69, 78, 94, 45, 45, 91, 86]
@@ -16,31 +18,33 @@ def test_spatial_csv_bool_no_color():
 
 def test_spatial_csv_no_bool_color():
     spatial_csv_data = get_plotting_data.SpatialCsvData.from_csv(
-        "tests/test_data/OnOff-X-Y-Timestamp.csv", DataStorage.COLOR)
+        "tests/test_data/OnOff-X-Y-Timestamp.csv", DataStorage.COLOR
+    )
 
     assert spatial_csv_data.polarities == []
     assert spatial_csv_data.x_positions == [82, 17, 86, 69, 78, 94, 45, 45, 91, 86]
     assert spatial_csv_data.y_positions == [78, 71, 37, 104, 75, 30, 12, 12, 32, 84]
     assert spatial_csv_data.timestamps == [0, 4, 6, 8, 9, 9, 10, 11, 17, 19]
-    assert spatial_csv_data.polarities_color == ['g', 'r', 'r', 'g', 'g', 'g', 'g', 'g', 'r', 'r']
+    assert spatial_csv_data.polarities_color == ["g", "r", "r", "g", "g", "g", "g", "g", "r", "r"]
 
 
 def test_spatial_csv_bool_color():
     spatial_csv_data = get_plotting_data.SpatialCsvData.from_csv(
-        "tests/test_data/OnOff-X-Y-Timestamp.csv", DataStorage.BOOL_AND_COLOR)
+        "tests/test_data/OnOff-X-Y-Timestamp.csv", DataStorage.BOOL_AND_COLOR
+    )
 
     assert spatial_csv_data.polarities == [True, False, False, True, True, True, True, True, False, False]
     assert spatial_csv_data.x_positions == [82, 17, 86, 69, 78, 94, 45, 45, 91, 86]
     assert spatial_csv_data.y_positions == [78, 71, 37, 104, 75, 30, 12, 12, 32, 84]
     assert spatial_csv_data.timestamps == [0, 4, 6, 8, 9, 9, 10, 11, 17, 19]
-    assert spatial_csv_data.polarities_color == ['g', 'r', 'r', 'g', 'g', 'g', 'g', 'g', 'r', 'r']
+    assert spatial_csv_data.polarities_color == ["g", "r", "r", "g", "g", "g", "g", "g", "r", "r"]
 
 
-# TODO implement me (OnOff-X-Y.csv)
 def test_incorrect_format():
-    pass
+    with pytest.raises(ValueError, match="CSV may not be the correct format.\nHeader should be On/Off,X,Y,Timestamp"):
+        get_plotting_data.SpatialCsvData.from_csv("tests/test_data/OnOff-X-Y.csv", DataStorage.COLOR)
 
 
-# TODO implement me (OnOff-X-Y-Timestamp-NODATA.csv)
 def test_empty_csv():
-    pass
+    with pytest.raises(ValueError, match="CSV file 'tests/test_data/OnOff-X-Y-Timestamp-NODATA.csv' seems to be empty"):
+        get_plotting_data.SpatialCsvData.from_csv("tests/test_data/OnOff-X-Y-Timestamp-NODATA.csv", DataStorage.COLOR)
