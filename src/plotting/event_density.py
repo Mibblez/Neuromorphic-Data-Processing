882ec33bcea12a66f06749096c729ecbfad797a7
Anthony Beninati <abeninati01@manhattan.edu>
2023-01-28 19:08:35 -0500

    refactor command line args

--- src/plotting/event_density.py
+++ src/plotting/event_density.py
@@ -5,87 +5,61 @@ import matplotlib.pyplot as plt
 from plotting_utils import filename_regex
 import argparse
 import os
-import sys
+import math
+from plotting_utils.plotting_helper import path_arg, file_arg, int_arg_not_negative, int_arg_positive_nonzero
 
 
-pixel_x = -1
-pixel_y = -1
-
-area_size = -1
-max_plot_points = float("inf")
-
-csv_filename = ""
-save_directory = ""
-
-
-def get_args():
-    global pixel_x, pixel_y, area_size, max_plot_points, csv_filename, save_directory
-
+def get_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
-    parser.add_argument("aedat_csv_file", help="CSV with AEDAT data to plot", type=str)
+    parser.add_argument("aedat_csv_file", help="CSV with AEDAT data to plot", type=file_arg)
 
-    parser.add_argument("--pixel_x", "-x", help="x coordinate of desired pixel", type=int, required=True)
-    parser.add_argument("--pixel_y", "-y", help="y coordinate of desired pixel", type=int, required=True)
-    parser.add_argument("--area_size", "-a", help="size of box around pixel to observe", type=int, required=True)
-    parser.add_argument("--max_plot_points", "-m", help="max number of points to plot", type=int)
-    parser.add_argument("--save_directory", "-d", help="Save file to directory", type=str)
+    parser.add_argument(
+        "--pixel_x", "-x", help="x coordinate of desired pixel", type=int_arg_not_negative, required=True
+    )
+    parser.add_argument(
+        "--pixel_y", "-y", help="y coordinate of desired pixel", type=int_arg_not_negative, required=True
+    )
+    parser.add_argument(
+        "--area_size", "-a", help="size of box around pixel to observe", type=int_arg_positive_nonzero, required=True
+    )
+    parser.add_argument(
+        "--max_plot_points",
+        "-m",
+        help="max number of points to plot",
+        default=math.inf,
+        type=int_arg_positive_nonzero,
+    )
+    parser.add_argument("--save_directory", "-d", help="Save file to directory", default=".", type=path_arg)
 
-    args = parser.parse_args()
-
-    if args.save_directory is not None:
-        if not os.path.exists(args.save_directory):
-            sys.exit(f'Error: Specified path "{args.save_directory}" does not exist')
-        else:
-            save_directory = args.save_directory
-
-    csv_filename = args.aedat_csv_file
-
-    if not os.path.exists(csv_filename):
-        quit(f"the file you have passed, {csv_filename}, does not exist")
-    pixel_x = args.pixel_x
-    if pixel_x < 0:
-        quit("pixel_x coordinate was negative, it should be a positive integer")
-    pixel_y = args.pixel_y
-    if pixel_y < 0:
-        quit("pixel_y coordinate was negative, it should be positive integer")
-    area_size = args.area_size
-    if area_size < 0:
-        quit("area_size was negative, it should be positive integer")
-    if args.max_plot_points is not None and args.max_plot_points > 0:
-        max_plot_points = args.max_plot_points
+    return parser.parse_args()
 
 
-if __name__ == "__main__":
-    get_args()
+def main(args: argparse.Namespace):
     matplotlib.use("Qt5Agg")
 
     last_pixel_state = None
     redundancies = 0  # TODO: do redundancies for all pixels
-    reset_pixel = True
 
     change_timestamps = []  # The times when the pixel changed state
     time_between = []  # The times between the state changes
 
-    with open(csv_filename) as csv_file:
+    with open(args.aedat_csv_file) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=",")
 
         for row in islice(csv_reader, 1, None):  # Skip the header
             x_row = int(row[1])
             y_row = int(row[2])
 
-            check_x = abs(x_row - pixel_x)
-            check_y = abs(y_row - pixel_y)
+            check_x = abs(x_row - args.pixel_x)
+            check_y = abs(y_row - args.pixel_y)
 
-            if check_x < area_size and check_y < area_size:
+            if check_x < args.area_size and check_y < args.area_size:
                 pixel_state = (row[0] == "True") or (row[0] == "1")
 
                 if pixel_state != last_pixel_state:
-                    reset_pixel = False
                     last_pixel_state = pixel_state
                     change_timestamps.append(float(row[3]))
-                    if len(change_timestamps) > max_plot_points:
-                        print(redundancies, "broken")
-
+                    if len(change_timestamps) > args.max_plot_points:
                         break
                 else:
                     redundancies += 1
@@ -108,17 +82,22 @@ if __name__ == "__main__":
     plt.title("Temporal Resoltion")
     plt.xlabel("Time(mS)")
 
-    hz = filename_regex.parse_frequency(csv_filename, "Hz_")
-    voltage = filename_regex.parse_voltage(csv_filename, "V_")
-    waveform_type = filename_regex.parse_waveform(csv_filename, "_")
-    degrees = filename_regex.parse_degrees(csv_filename, "_DegreesPolarized")
+    hz = filename_regex.parse_frequency(args.aedat_csv_file, "Hz_")
+    voltage = filename_regex.parse_voltage(args.aedat_csv_file, "V_")
+    waveform_type = filename_regex.parse_waveform(args.aedat_csv_file, "_")
+    degrees = filename_regex.parse_degrees(args.aedat_csv_file, "_DegreesPolarized")
 
     # TODO: what if the file is specified as polarized but no angle is given?
 
     if hz == "" and degrees == "":
         print("WARNING: Could not infer polarizer angle or frequency from file name")
 
-    plt.savefig(os.path.join(save_directory, f"{hz}{voltage}{waveform_type}{degrees}_event_density.png"))
+    plt.savefig(os.path.join(args.save_directory, f"{hz}{voltage}{waveform_type}{degrees}_event_density.png"))
 
     if len(time_between) != 0:
         print(f"Average time between: {round(sum(time_between) / len(time_between), 2)}mS")
+
+
+if __name__ == "__main__":
+    args = get_args()
+    main(args)
