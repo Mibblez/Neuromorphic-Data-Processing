b8ffc7ffe4cb100d01469666b0b7e3902226adad
ggojcaj626 <ggojcaj626@gmail.com>
2023-02-21 22:59:39 -0500

    Update wavelet_decomposition.py
    added arg parsing

--- src/image_processing/wavelet_decomposition.py
+++ src/image_processing/wavelet_decomposition.py
@@ -3,25 +3,49 @@ import matplotlib.pyplot as plt
 import matplotlib
 import cv2
 
+import ntpath
+import argparse
+import os
+
 import pywt
 import pywt.data
 
+from plotting_utils.plotting_helper import path_arg, file_arg_image
 
-def wavelet_decomposition(img: np.ndarray, wavelet_type: str = "db4"):
+
+def get_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("image_path", help="path to image to perform wavelet decomposition on", type=file_arg_image)
+    parser.add_argument(
+        "--type",
+        "-t",
+        help="sets the type of wavelet decomposition to perform",
+        action="store",
+        type=str
+    )
+    parser.add_argument("--save_directory", "-d", help="Save file to directory", type=path_arg, default=".")
+    args = parser.parse_args()
+    if args.type not in pywt.wavelist():
+        parser.error("Error: Invalid wavelet type")
+
+    return parser.parse_args()
+
+
+def wavelet_decomposition(img: np.ndarray, wavelet_type: str):
     coeffs2 = pywt.dwt2(img, wavelet_type)
     LL, (LH, HL, HH) = coeffs2
 
     return LL, LH, HL, HH
 
 
-if __name__ == "__main__":
+def main(args: argparse.Namespace):
     matplotlib.use("TkAgg")
 
-    original = cv2.imread("whel.png")
-    original = cv2.cvtColor(original, cv2.COLOR_RGB2GRAY)
+    image = cv2.imread(args.image_path)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 
-    LL, LH, HL, HH = wavelet_decomposition(original)
-    print(LL.shape)
+    LL, LH, HL, HH = wavelet_decomposition(image, args.type)
 
     # Wavelet transform of image, and plot approximation and details
     titles = ["Approximation", " Horizontal detail", "Vertical detail", "Diagonal detail"]
@@ -35,5 +59,12 @@ if __name__ == "__main__":
         ax.set_xticks([])
         ax.set_yticks([])
 
+    image_name = os.path.splitext(ntpath.basename(args.image_path))[0]
+
     fig.tight_layout()
-    plt.show()
+    plt.savefig(os.path.join(args.save_directory, f"WaveletDecomp-{args.type}-{image_name}.png"))
+
+
+if __name__ == "__main__":
+    args = get_args()
+    main(args)
