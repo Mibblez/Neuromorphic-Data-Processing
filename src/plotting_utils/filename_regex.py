d19d9d9837a0683f3d435896212da7f47d6730ed
Anthony Beninati <abeninati01@manhattan.edu>
2024-05-14 23:31:17 -0400

    update regex

--- src/plotting_utils/filename_regex.py
+++ src/plotting_utils/filename_regex.py
@@ -2,7 +2,7 @@ import re
 
 
 def parse_frequency(input_str: str, append_if_found: str = "") -> str:
-    frequency = re.search("[0-9]+\s?hz", input_str, re.IGNORECASE)
+    frequency = re.search(r"\d+\s*hz", input_str, re.IGNORECASE)
 
     if frequency:
         return frequency.group().strip(" hzHZ") + append_if_found
@@ -11,7 +11,7 @@ def parse_frequency(input_str: str, append_if_found: str = "") -> str:
 
 
 def parse_voltage(input_str: str, append_if_found: str = "") -> str:
-    voltage_match = re.search("[0-9]{1,} ?m?v", input_str, re.IGNORECASE)
+    voltage_match = re.search(r"\d+\s*m?v", input_str, re.IGNORECASE)
 
     if voltage_match:
         voltage_str = voltage_match.group().lower()
@@ -25,12 +25,12 @@ def parse_voltage(input_str: str, append_if_found: str = "") -> str:
 
 
 def parse_waveform(input_str: str, append_if_found: str = "") -> str:
-    waveform = re.search("(burst|sine|square|triangle|noise|dc)", input_str, re.IGNORECASE)
+    waveform = re.search(r"(burst|sine|square|triangle|noise|dc)", input_str, re.IGNORECASE)
     return waveform.group() + append_if_found if waveform else ""
 
 
 def parse_degrees(input_str: str, append_if_found: str = "") -> str:
-    degrees = re.search("[0-9]{1,} ?deg", input_str, re.IGNORECASE)
+    degrees = re.search(r"\d+\s*deg", input_str, re.IGNORECASE)
 
     if degrees:
         return degrees.group().strip(" degDEG") + append_if_found
@@ -39,7 +39,7 @@ def parse_degrees(input_str: str, append_if_found: str = "") -> str:
 
 
 def parse_slots(input_str: str, append_if_found: str = "") -> str:
-    slots = re.search("[0-9]{1,}sl", input_str, re.IGNORECASE)
+    slots = re.search(r"\d+sl", input_str, re.IGNORECASE)
 
     if slots:
         return slots.group().strip(" slSL") + append_if_found
@@ -48,7 +48,7 @@ def parse_slots(input_str: str, append_if_found: str = "") -> str:
 
 
 def parse_threshold(input_str: str, append_if_found: str = "") -> str:
-    threshold_match = re.search("m?[0-9]{1,}t(hreshold)?", input_str, re.IGNORECASE)
+    threshold_match = re.search(r"m?\d+t(hreshold)?", input_str, re.IGNORECASE)
 
     if threshold_match:
         threshold_str = threshold_match.group().lower().replace("m", "-").strip("threshold")
@@ -58,7 +58,7 @@ def parse_threshold(input_str: str, append_if_found: str = "") -> str:
 
 
 def parse_intensity(input_str: str, append_if_found: str = "") -> str:
-    intensity_match = re.search("(0.1int|0.5int|1.0int)", input_str, re.IGNORECASE)
+    intensity_match = re.search(r"\d+\.?\d*int", input_str, re.IGNORECASE)
 
     if intensity_match:
         intensity_str = intensity_match.group().lower().replace("m", "-").strip("int")
@@ -68,7 +68,7 @@ def parse_intensity(input_str: str, append_if_found: str = "") -> str:
 
 
 def parse_nthreshold(input_str: str, append_if_found: str = "") -> str:
-    nthreshold_match = re.search("n?[0-9]{1,}t(hreshold)?", input_str, re.IGNORECASE)
+    nthreshold_match = re.search(r"n?\d+t(hreshold)?", input_str, re.IGNORECASE)
 
     if nthreshold_match:
         nthreshold_str = nthreshold_match.group().lower().strip("threshold")
@@ -78,7 +78,7 @@ def parse_nthreshold(input_str: str, append_if_found: str = "") -> str:
 
 
 def parse_retAngle(input_str: str, append_if_found: str = "") -> str:
-    retAngel_match = re.search("[0-9]{1,}R", input_str, re.IGNORECASE)
+    retAngel_match = re.search(r"\d+R", input_str, re.IGNORECASE)
 
     if retAngel_match:
         retAngel_str = retAngel_match.group().strip("R")
